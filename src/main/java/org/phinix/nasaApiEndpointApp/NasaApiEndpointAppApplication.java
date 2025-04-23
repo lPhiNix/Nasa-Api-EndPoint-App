b8410484bd0050b1ad37a7a17df89dbc24951ed5
lPhiNix <pablomargamer05@gmail.com>
2025-04-23 13:21:55 +0200

    First Commit

---
+++ src/main/java/org/phinix/nasaApiEndpointApp/NasaApiEndpointAppApplication.java
@@ -0,0 +1,13 @@
+package org.phinix.nasaApiEndpointApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaApiEndpointAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaApiEndpointAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/phinix/nasaApiEndpointApp/NasaApiEndpointAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.phinix.nasaApiEndpointApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaApiEndpointAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
