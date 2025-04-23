63634ff195ddeda802dc2672db96bca659e8b96c
lPhiNix <pablomargamer05@gmail.com>
2025-04-23 13:50:40 +0200

    AsteroidsController Implemented (Api Implemented)

--- src/main/java/org/phinix/nasaApiEndpointApp/controller/AsteroidsController.java
+++ src/main/java/org/phinix/nasaApiEndpointApp/controller/AsteroidsController.java
@@ -1,4 +1,20 @@
 package org.phinix.nasaApiEndpointApp.controller;
 
+import org.phinix.nasaApiEndpointApp.service.AsteroidsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AsteroidsController {
+    private final AsteroidsService service;
+
+    public AsteroidsController(AsteroidsService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/asteroids")
+    public Object getAsteroids(@RequestParam int days) {
+        return service.getRawAsteroids(days);
+    }
 }
