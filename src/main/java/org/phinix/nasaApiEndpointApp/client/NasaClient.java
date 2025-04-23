f112fdd79a9efd1e03b433928bfed36873c2ade5
lPhiNix <pablomargamer05@gmail.com>
2025-04-23 13:48:06 +0200

    NasaClient y AsteroidsService Implemented

--- src/main/java/org/phinix/nasaApiEndpointApp/client/NasaClient.java
+++ src/main/java/org/phinix/nasaApiEndpointApp/client/NasaClient.java
@@ -1,4 +1,29 @@
 package org.phinix.nasaApiEndpointApp.client;
 
+import org.phinix.nasaApiEndpointApp.dto.external.NasaApiResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
 public class NasaClient {
+    private final RestTemplate restTemplate;
+    private final String apiUrl;
+    private final String apiKey;
+
+    public NasaClient(
+            RestTemplate restTemplate,
+            @Value("${nasa.api.url}") String apiUrl,
+            @Value("${nasa.api.key}") String apiKey
+    ) {
+        this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
+        this.apiKey = apiKey;
+    }
+
+    public NasaApiResponse getAsteroidsFeed(String startDate, String endDate) {
+        String url = String.format("%s?start_date=%s&end_date=%s&api_key=%s",
+                apiUrl, startDate, endDate, apiKey);
+        return restTemplate.getForObject(url, NasaApiResponse.class);
+    }
 }
--- src/main/java/org/phinix/nasaApiEndpointApp/service/AsteroidsService.java
+++ src/main/java/org/phinix/nasaApiEndpointApp/service/AsteroidsService.java
@@ -1,4 +1,28 @@
 package org.phinix.nasaApiEndpointApp.service;
 
+import org.phinix.nasaApiEndpointApp.client.NasaClient;
+import org.phinix.nasaApiEndpointApp.dto.external.NasaApiResponse;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Service
 public class AsteroidsService {
+    private final NasaClient client;
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public AsteroidsService(NasaClient client) {
+        this.client = client;
+    }
+
+    public NasaApiResponse getRawAsteroids(int days) {
+        LocalDate startDate = LocalDate.now();
+        LocalDate endDate = startDate.plusDays(days);
+
+        return client.getAsteroidsFeed(
+                startDate.format(dateFormatter),
+                endDate.format(dateFormatter)
+        );
+    }
 }
