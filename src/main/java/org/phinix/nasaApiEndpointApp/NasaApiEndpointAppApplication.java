09c6c556ec12d5d0d59af25f6b45a3d84a70a5b6
lPhiNix <pablomargamer05@gmail.com>
2025-04-24 11:49:04 +0200

    Minimal Changes

--- src/main/java/org/phinix/nasaApiEndpointApp/NasaApiEndpointAppApplication.java
+++ src/main/java/org/phinix/nasaApiEndpointApp/NasaApiEndpointAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NasaApiEndpointAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NasaApiEndpointAppApplication.class, args);
 	}
-
 }
