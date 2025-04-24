53b6ba5fcb4638b9759ea5f574ce9ae7321e3a17
lPhiNix <pablomargamer05@gmail.com>
2025-04-24 08:40:17 +0200

    Response DTO Implemented

--- src/main/java/org/phinix/nasaApiEndpointApp/dto/response/NasaAsteroidsResponseDTO.java
+++ src/main/java/org/phinix/nasaApiEndpointApp/dto/response/NasaAsteroidsResponseDTO.java
@@ -1,6 +1,9 @@
 package org.phinix.nasaApiEndpointApp.dto.response;
 
 public record NasaAsteroidsResponseDTO(
-
-) {
-}
+    String name,
+    double diameter,
+    double speed,
+    String date,
+    String planet
+) {}
